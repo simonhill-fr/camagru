228d3ef606c5e327bf6198bf00a18042eccdeb67
Simon HILL <shill@e3r8p1.42.fr>
2016-12-21 21:38:20 +0100

    Fixed several minor shit. Need to clean the ajax in create.php as it's throwing errors in firefox console! Need to finish checklist

--- sign_up_success.php
+++ sign_up_success.php
@@ -17,6 +17,6 @@ include 'error_report.php';
 	<h1>Verify Email</h1>
 	<fieldset>
 		<legend><span class="number">2</span> Activation link was sent</legend>
-		Activate account, then <a href="../log_in.php">login</a>
+		Activate account, then <a href="log_in.php">login</a>
 	</fieldset>	
 </form>
